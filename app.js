a389138af0fff3cc110df59b77a181c75897e439
fnx <fangnaxin@gmail.com>
2018-12-30 16:22:53 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue ({
+  el: '#app',
+  data: {
+    in_start: '有意思吗？你们每天就知道刷这些烂梗，像个复读机',
+    in_mention: '复读机',
+    in_remember: '西游记中的唐僧',
+    in_role: '美猴王孙悟空',
+    in_artxingxiang: '美猴王',
+    in_xingxiang: '正能量',
+    in_culture: '中华'
+
+  }
+})
